a7926a6c03fad8e63becb36e7880308ad862593a
Diomidis Spinellis <dds@aueb.gr>
2016-07-31 17:15:50 +0300

    Include media files in the deploy directory
    To avoid duplication store presentations and handouts in the same
    directory, with presentation files suffixed with -p.

--- tools/build.sh
+++ tools/build.sh
@@ -13,9 +13,9 @@ for course in $COURSES ; do
     input=courses/$file
 
     # Prepare the leaf presentation file
-    pres_out=deploy_dir/pres/$(dirname $file)/$(basename $file .md).html
+    pres_out=deploy_dir/$(dirname $file)/$(basename $file .md)-p.html
     if [ ! -r $pres_out -o $input -nt $pres_out ] ; then
-      mkdir -p deploy_dir/pres/$(dirname $file)
+      mkdir -p deploy_dir/$(dirname $file)
       echo Building $pres_out
       {
 	sed "s/TITLE_HERE/$title/" assets/presentation-top.html
@@ -25,9 +25,10 @@ for course in $COURSES ; do
 
     fi
 
-    hand_out=deploy_dir/handouts/$(dirname $file)/$(basename $file .md).html
+    # Prepare handout file
+    hand_out=deploy_dir/$(dirname $file)/$(basename $file .md).html
     if [ ! -r $hand_out -o $input -nt $hand_out ] ; then
-      mkdir -p deploy_dir/handouts/$(dirname $file)
+      mkdir -p deploy_dir/$(dirname $file)
       echo Building $hand_out
       {
 	sed "s/TITLE_HERE/$title/" assets/index-top.html
@@ -41,14 +42,17 @@ for course in $COURSES ; do
     fi
   done
 
+  # Deploy media files
+  rsync -a --delete courses/$course/media deploy_dir/$course/
+
   # Prepare the course index files
-  mkdir -p deploy_dir/{pres,handouts}
-  {
-    sed "s/TITLE_HERE/Coding Bootcamp: Table of contents/" assets/index-top.html
-    pandoc -f markdown_github -t html courses/${course}.md |
-    sed '/href/s/\.md"/.html"/'
-    cat assets/index-bottom.html
-  } |
-  tee deploy_dir/pres/${course}.html >deploy_dir/handouts/${course}.html
+  for hp in '' -p ; do
+    {
+      sed "s/TITLE_HERE/Coding Bootcamp: Table of contents/" assets/index-top.html
+      pandoc -f markdown_github -t html courses/${course}.md |
+      sed '/href/s/\.md"/'$hp'.html"/'
+      cat assets/index-bottom.html
+    } >deploy_dir/${course}${hp}.html
+  done
 
 done
