a8b45274830064c75c1ec65dce40cf7e52d5327d
Diomidis Spinellis <dds@aueb.gr>
2016-08-01 09:48:32 +0300

    Ensure build can be executed on Travis CI

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  tools/build.sh
+  sh tools/build.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
