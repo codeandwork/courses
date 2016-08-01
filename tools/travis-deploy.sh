7ca399703270f6326dba3f8c50cf87e82a1367ef
Diomidis Spinellis <dds@aueb.gr>
2016-08-01 11:11:31 +0300

    Don't log credentials

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -53,12 +53,20 @@ fi
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
+# Don't display credentials
+set +x
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+
+if [ -n "$DEBUG" ] ; then
+  set -x
+fi
+
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
